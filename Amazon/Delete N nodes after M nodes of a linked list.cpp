5e2dd2220b2075ddf83d8b71d91ac08f0581389e
likitd <likitbdvt@gmail.com>
2025-01-09 23:24:03 +0530

    amazon1

---
+++ Amazon/Delete N nodes after M nodes of a linked list.cpp
@@ -0,0 +1,24 @@
+Node* linkdelete(Node* head, int n, int m) {
+        // code here
+        Node* node = head;
+        while(node!=nullptr)
+        {
+            Node* prev = nullptr;
+            // Skip m nodes
+            for(size_t id=0; id<m && node!=nullptr; id++)
+            {
+                prev = node;
+                node = node->next;
+            }
+            
+            // Delete n nodes
+            for(size_t id=0; id<n && node!=nullptr; id++)
+            {
+                Node* temp = node;
+                node = node->next;
+                delete temp;
+            }
+            prev->next = node;
+        }
+        return head;
+    }
